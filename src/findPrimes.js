27a5958187eef0fca2156d1fb24186824da45c53
Sam Ellenrieder <sam.ell@web.de>
2017-11-12 15:47:36 +0000

    7. prime finder actually works

--- src/findPrimes.js
+++ src/findPrimes.js
@@ -6,49 +6,30 @@ function FindPrimes() {
 
 FindPrimes.prototype.prime = function(number) {
   if(number === 2) {
-
     this.primeNumbers.push(3)
-
   } else if(number % 2 !== 0 && number % 3 !== 0 && number !== 3 ) {
-
     return(true)
-  	// this.primeNumbers.push(number)
   } else {
-
   	return(false)
-
   };
 };
-
 
 //this number loops and finds prime numbers until the array is as long as the user requested
 
 FindPrimes.prototype.collectPrimes = function(usernumber) {
-  if(usernumber > 1) {
-  	
-  	var n = 5
-  	
+  if(usernumber === 1) {	
+  	return this.primeNumbers
+  } else {
+  	var n = 3
   	while(usernumber > this.primeNumbers.length){
-      
-      if(this.prime(n) === true)
-      
+      if(this.prime(n)){
       this.primeNumbers.push(n)
   	}
-  	
-  	n++
-
-  } else {
-
-    return this.primeNumbers
-
+  	  n++;
+    }
   }
-
-  return this.primeNumbers
-
+   return this.primeNumbers
 };
-
-
-
 
 
 module.exports.FindPrimes = FindPrimes;
