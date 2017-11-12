573fc75f3cd7326dc1ffaf91f893c2cf63565165
Sam Ellenrieder <sam.ell@web.de>
2017-11-12 17:06:57 +0000

    9. collectPrimes tests

--- spec/findPrimesSpec.js
+++ spec/findPrimesSpec.js
@@ -9,9 +9,19 @@ describe('FindPrimes', function() {
   it('has a prime array', function() {
     expect(n.primeNumbers).toEqual([2]);
   });
+
+  describe('collectPrimes', function(){
     
-  it('pushes 3 into the array when user wants 2 primes', function() {
-    expect(n.collectPrimes(3)).toEqual([ 2, 3, 5 ]);
+    it('pushes 3 into the array when user wants 2 primes', function() {
+      expect(n.collectPrimes(2)).toEqual([ 2, 3 ]);
+    });
+
+    it('creates an array of more than 1 prime if the user wants', function(){
+      expect(n.collectPrimes(5)).toEqual([ 2, 3, 5, 7, 11 ])
+    });
+
+    it('returns unchanged primeNumbers arra if user enters 1', function(){
+      expect(n.collectPrimes(1)).toEqual([2])
+    });
   });
 });
-
