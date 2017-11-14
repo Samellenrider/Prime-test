e3fd153aa1cea97af143ebc907e56b1208abdfa7
Sam Ellenrieder <sam.ell@web.de>
2017-11-14 09:45:01 +0000

    20. formatTable test

--- spec/formatTableSpec.js
+++ spec/formatTableSpec.js
@@ -10,11 +10,11 @@ describe('FormatTable', function() {
     n = new FormatTable();
   });
 
-  describe('tableFormatter', function() {
-
-    it('removes square brackets & commas & formats the table', function() {
-      console.dir(n);
-      expect(n.tableFormatter(table)).toEqual(" |  2|  3|  5|", "2|  4|  6|  10|", "3|  6|  9|  15|");
+  it('removes square brackets & commas & formats the table', function() {
+    it('tests', function() {
+      spyOn(console, 'dir');
+      n.tableFormatter(table)
+      expect(console.dir ).toEqual(" |  2|  3|  5|", "2|  4|  6|  10|", "3|  6|  9|  15|");
     });
   });
 });
